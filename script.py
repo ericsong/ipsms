3eb8699bdf0935a13a15c8ed101ca89fa0ec1772
ericsong <regonics@gmail.com>
2015-12-25 17:07:43 -0500

    sends ip address to configured number

--- script.py
+++ script.py
@@ -1,6 +1,22 @@
 import requests
+import os
+import configparser
+from twilio.rest import TwilioRestClient
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+account = config['TWILIO']['ACC']
+token = config['TWILIO']['TOKEN']
+client = TwilioRestClient(account, token)
 
 r = requests.get('http://icanhazip.com')
 ip = r.text.strip()
+
+message = client.messages.create(
+    to=config['PHONE_NUMBERS']['TO'],
+    from_=config['PHONE_NUMBERS']['FROM'],
+    body=ip
+)
 
 print(ip)
