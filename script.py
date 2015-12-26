0a0709ac2a73b0090166d113d4dbefb96e4490b3
ericsong <regonics@gmail.com>
2015-12-26 13:30:35 -0500

    basic functionality complete.

--- script.py
+++ script.py
@@ -1,7 +1,11 @@
 import requests
 import os
 import configparser
+from twisted.internet import task
+from twisted.internet import reactor
 from twilio.rest import TwilioRestClient
+
+CURRENT_IP = ''
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -10,13 +14,27 @@ account = config['TWILIO']['ACC']
 token = config['TWILIO']['TOKEN']
 client = TwilioRestClient(account, token)
 
-r = requests.get('http://icanhazip.com')
-ip = r.text.strip()
+def getCurrentIp():
+    r = requests.get('http://icanhazip.com')
+    ip = r.text.strip()
+    return ip
 
-message = client.messages.create(
-    to=config['PHONE_NUMBERS']['TO'],
-    from_=config['PHONE_NUMBERS']['FROM'],
-    body=ip
-)
+def sendIpSMS(ip):
+    message = client.messages.create(
+        to=config['PHONE_NUMBERS']['TO'],
+        from_=config['PHONE_NUMBERS']['FROM'],
+        body=ip
+    )
 
-print(ip)
+def checkIp():
+    global CURRENT_IP
+
+    ip = getCurrentIp()
+    print(ip)
+    if ip != CURRENT_IP:
+        sendIpSMS(ip)
+        CURRENT_IP = ip
+
+l = task.LoopingCall(checkIp)
+l.start(60)
+reactor.run()
